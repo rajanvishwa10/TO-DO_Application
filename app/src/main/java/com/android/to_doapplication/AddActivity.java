770392bbf109a440c7fa46afe3e4ccefb1f7df11
rajanvishwa10 <rajanvishwa10@gmail.com>
2021-02-14 22:53:58 +0530

    Added Firebase database and authentication

--- app/src/main/java/com/android/to_doapplication/AddActivity.java
+++ app/src/main/java/com/android/to_doapplication/AddActivity.java
@@ -6,23 +6,38 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class AddActivity extends AppCompatActivity {
     Toolbar toolbar;
     ConstraintLayout constraintLayout;
     CardView cardView;
+    String title = "", note = "";
     ImageButton imageButton, imageButton2;
     LinearLayout linearLayout;
+    String color = "#FFFFFFFF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,14 +74,54 @@ public class AddActivity extends AppCompatActivity {
     }
 
     public void addYellowBackgroundColor(View view) {
+        color = "#FFEB3B";
         constraintLayout.setBackgroundColor(Color.parseColor("#FFEB3B"));
     }
 
     public void addRedBackgroundColor(View view) {
+        color = "#FF0000";
         constraintLayout.setBackgroundColor(Color.parseColor("#FF0000"));
     }
 
     public void addOrangeBackgroundColor(View view) {
+        color = "#FF4213";
         constraintLayout.setBackgroundColor(Color.parseColor("#FF4213"));
+    }
+
+    public void addImage(View view) {
+        Toast.makeText(this, "Add Image", Toast.LENGTH_SHORT).show();
+    }
+
+    public void submit(View view) {
+        EditText editText = findViewById(R.id.title);
+        EditText editText2 = findViewById(R.id.note);
+        title = editText.getText().toString().trim();
+        note = editText2.getText().toString().trim();
+        addTODatabase(title, note, color);
+    }
+
+    private void addTODatabase(String title, String note, String color) {
+        final Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault());
+        final String formattedDate = df.format(c);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("title", title);
+        hashMap.put("note", note);
+        hashMap.put("color", color);
+        hashMap.put("date", formattedDate);
+
+        databaseReference.child("User").
+                child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(formattedDate)
+                .updateChildren(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(AddActivity.this, "Submited", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/android/to_doapplication/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.android.to_doapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity {
+    GoogleSignInClient googleSignInClient;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        createRequest();
+    }
+
+    private void createRequest() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    private void signIn() {
+        Intent intent  = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent,2);
+    }
+
+    public void googleLogin(View view) {
+        signIn();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "Sign in Successful", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
